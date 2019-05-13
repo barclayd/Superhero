5820c2e4a2d3146099ee14d8aa74ad1ff3fa6b60
Daniel Barclay <daniel@urbanintel.io>
2019-05-13 19:06:46 +0100

    Updates

---
+++ precache-manifest.dd53bd0d0350099c1d04622d012c938d.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "264d32079873f0620bf59e4d7945ca89",
+    "url": "/barclayd/Superhero/index.html"
+  },
+  {
+    "revision": "da107b3d3579af49b8c6",
+    "url": "/barclayd/Superhero/static/css/2.b960bd9d.chunk.css"
+  },
+  {
+    "revision": "9e633c7fbd90508b989e",
+    "url": "/barclayd/Superhero/static/css/main.98569f47.chunk.css"
+  },
+  {
+    "revision": "da107b3d3579af49b8c6",
+    "url": "/barclayd/Superhero/static/js/2.a419de4c.chunk.js"
+  },
+  {
+    "revision": "9e633c7fbd90508b989e",
+    "url": "/barclayd/Superhero/static/js/main.843251b4.chunk.js"
+  },
+  {
+    "revision": "c9bd5775de1bbef11ffb",
+    "url": "/barclayd/Superhero/static/js/runtime~main.50202a44.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/barclayd/Superhero/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/barclayd/Superhero/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/barclayd/Superhero/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/barclayd/Superhero/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/barclayd/Superhero/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/barclayd/Superhero/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/barclayd/Superhero/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/barclayd/Superhero/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/barclayd/Superhero/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/barclayd/Superhero/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/barclayd/Superhero/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/barclayd/Superhero/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/barclayd/Superhero/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/barclayd/Superhero/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/barclayd/Superhero/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/barclayd/Superhero/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
